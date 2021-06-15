bdce45432475e8724c754258d0f1ff428843d3c0
hexovlad <vlad.savescu@redbaronsecurity.com>
2021-06-15 13:38:25 +0300

    Added the PS1 format

--- installer.sh
+++ installer.sh
@@ -10,6 +10,22 @@ function start_message(){
     echo -en "Installing basic config... \n"
 }
 
+
+# Get the PS1
+function PS1_setter(){
+	echo "if [ $EUID != 0 ] ; then
+
+    export PS1="\[$(tput bold)\]\[\033[38;5;9m\]\u\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;245m\]@\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;46m\]\H\[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \A\n[\[$(tput sgr0)\]\[\033[38;5;8m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]]\\$\[$(tput sgr0)\] "
+
+else
+
+    export PS1='\e[1;38;5;56;48;5;234m\u \e[38;5;240mon \e[1;38;5;28;48;5;234m\h \e[38;5;54m\d \@\e[0m\n\e[0;38;5;56;48;5;234m[\w] \e[1m\$\e[0m '
+
+" >> ~/.bashrc
+}
+
+
+# Installs Vim config
 function install_vim(){
 	# Getting vim-plug
 	# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim	
@@ -47,6 +63,7 @@ function main(){
     # echo -en "Installing the shortcuts...\n"
     # install_shortcuts
 }
+
 
 # Runs the main function 
 main
